cbd9f2dd32e04d3498a00cb1a058334b6ded148c
Nitiksh Kumar <122013711+nitikshh@users.noreply.github.com>
2024-06-23 07:47:53 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,8 +2,7 @@ from flask import Flask, request, jsonify, send_from_directory, render_template
 from pytube import YouTube
 import random
 import os
-import moviepy.editor as mp
-from PIL import Image
+import ffmpeg
 
 app = Flask(__name__)
 
@@ -15,8 +14,8 @@ def download_youtube_video(url, download_path="downloads"):
 
 def extract_clips(video_path, clip_duration=58, num_clips=10):
     clip_paths = []
-    video = mp.VideoFileClip(video_path)
-    video_duration = video.duration
+    video_info = ffmpeg.probe(video_path)
+    video_duration = float(video_info['format']['duration'])
 
     # Ensure clip duration doesn't exceed video duration
     clip_duration = min(clip_duration, video_duration)
@@ -25,25 +24,19 @@ def extract_clips(video_path, clip_duration=58, num_clips=10):
     start_points = random.sample(range(0, int(video_duration - clip_duration)), num_clips)
 
     for i, start_point in enumerate(start_points):
-        end_point = start_point + clip_duration
         clip_name = f"clip_{i+1}.mp4"
-        clip = video.subclip(start_point, end_point)
-        
-        # Resize the video to fit within a square frame by width
-        square_size = 1080  # typical size for a square video
-        resized_clip = clip.resize(width=square_size)
-        
-        # Add black bars to the top and bottom to make the video square
-        square_clip = resized_clip.on_color(size=(square_size, square_size),
-                                            color=(0, 0, 0),
-                                            pos=('center', 'center'))
-        
         output_clip_path = os.path.join("clips", clip_name)
-        square_clip.write_videofile(output_clip_path, codec="libx264")
+
+        (
+            ffmpeg
+            .input(video_path, ss=start_point, t=clip_duration)
+            .filter('scale', 1080, -1)
+            .output(output_clip_path)
+            .run(overwrite_output=True)
+        )
+
         clip_paths.append(output_clip_path)
     
-    # Close the video clip
-    video.close()
     return clip_paths
 
 @app.route('/')
